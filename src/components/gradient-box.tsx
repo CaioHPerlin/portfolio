fae0a7e55dac0ccb6d26aabcffc950c2490f06e6
CaioHPerlin <caiohperlin@gmail.com>
2025-05-24 16:00:00 -0400

    chore: remove unused gradientbox props

--- src/components/gradient-box.tsx
+++ src/components/gradient-box.tsx
@@ -1,13 +1,10 @@
-import type React from "react";
-
 interface GradientBoxProps {
   className?: string;
-  children?: React.ReactNode;
 }
 
 // This will be a gradient box for each section, I plan on including some
 // Noise to make it look neater.
-export default function GradientBox({ className, children }: GradientBoxProps) {
+export default function GradientBox({ className }: GradientBoxProps) {
   return (
     <div
       className={`bg-gradient-to-br from-background via-secondary to-primary rounded-2xl border-1 overflow-hidden relative ${className}`}
