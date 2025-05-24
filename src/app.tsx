7bd08950dc82aeb3613b6d5dd2bb2a89920edcdc
CaioHPerlin <caiohperlin@gmail.com>
2025-05-24 15:43:56 -0400

    feat: add basic styling to headings

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,10 @@
 export default function App() {
-  return <div className="">this is my app.</div>;
+  return (
+    <div className="flex flex-col w-[45rem] mx-auto text-center pt-[15vh]">
+      <div className="space-y-4">
+        <h1>Caio Hygino Perlin de Lima</h1>
+        <h2>Full-Stack Developer</h2>
+      </div>
+    </div>
+  );
 }
