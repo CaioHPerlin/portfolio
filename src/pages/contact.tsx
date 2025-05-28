603f3b837cc13fbf662f1d78b71910835d5dfa41
CaioHPerlin <caiohperlin@gmail.com>
2025-05-28 09:03:54 -0400

    chore: scaffold contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -2,9 +2,11 @@ import PageHeading from "../components/page-heading";
 
 export default function ContactPage() {
   return (
-    <PageHeading
-      title={"Get in Touch"}
-      subtitle={"Ideas Start with a Conversation"}
-    />
+    <div className="w-[45rem] mx-auto mt-20 py-10">
+      <PageHeading
+        title={"Get in Touch"}
+        subtitle={"Ideas Start with a Conversation"}
+      />
+    </div>
   );
 }
