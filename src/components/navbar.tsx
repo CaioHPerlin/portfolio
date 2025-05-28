f572b89effafbca6e39141e29ad266ab7a5eb917
CaioHPerlin <caiohperlin@gmail.com>
2025-05-28 09:03:42 -0400

    feat: improve navbar animation

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -11,10 +11,10 @@ function CustomLink({ to, children, ...props }: CustomLinkProps) {
       className={({ isActive }) => `
       ease-in-out text-xl tracking-wide transition-opacity
       hover:opacity-100!
-      after:block after:content-[''] after:border-b-2 after:border-accent
+      after:block after:content-[''] after:border-b-1 after:border-accent
       after:scale-x-0 after:transition-transform after:duration-150 
-      after:ease-in-out hover:after:scale-x-100
-          ${isActive ? "" : "opacity-80"}
+      after:ease-in-out hover:after:scale-x-100!
+          ${isActive ? "after:scale-x-100" : "opacity-80"}
       `}
       {...props}
     >
@@ -25,19 +25,10 @@ function CustomLink({ to, children, ...props }: CustomLinkProps) {
 
 export default function Navbar() {
   return (
-    <header className="py-6 fixed top-0 z-10 bg-[rgba(255, 255, 255, 0.2)] backdrop-blur-md w-full border-b-1 border-secondary/30">
-      <nav className="flex flex-row gap-8 w-[45rem] mx-auto opacity-100 hover:[&>*]:opacity-80 transition-opacity">
-        {/* 
-        
-        background: rgba(255, 255, 255, 0.2);
-border-radius: 16px;
-box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
-backdrop-filter: blur(5px);
--webkit-backdrop-filter: blur(5px);
-border: 1px solid rgba(255, 255, 255, 0.3);
-        */}
+    <header className="py-6 fixed top-0 z-10 bg-secondary/20 backdrop-blur-md w-full border-b-1 border-accent/20">
+      <nav className="flex flex-row gap-8 w-[80rem] mx-auto opacity-100 hover:[&>*]:opacity-80 hover:[&>*]:after:scale-x-0 transition-opacity">
         <CustomLink to="/">Myself</CustomLink>
-        <CustomLink to="/projects">Projects</CustomLink>
+        <CustomLink to="/portfolio">Portfolio</CustomLink>
         <CustomLink to="/contact">Contact</CustomLink>
       </nav>
     </header>
